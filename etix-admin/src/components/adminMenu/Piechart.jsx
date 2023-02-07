6e0ec865fc617b08d6be56f065774df9bb98bb8b
Jawde2000 <65705404+Jawde2000@users.noreply.github.com>
2023-02-08 01:37:19 +0800

    Update Piechart.jsx

--- etix-admin/src/components/adminMenu/Piechart.jsx
+++ etix-admin/src/components/adminMenu/Piechart.jsx
@@ -1,5 +1,6 @@
 import {React, useEffect, useState} from 'react';
 import { makeStyles} from '@material-ui/core/styles';
+import { Box,} from '@mui/material';
 import { Typography } from '@material-ui/core';
 import { Grid } from '@mui/material'
 import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
@@ -37,29 +38,50 @@ export default function Piechart() {
   useEffect(() => {
     dispatch(ChildrenGender());
     if (success) {
+      console.log(childrenDemo)
       getMale(childrenDemo[0])
       getFemale(childrenDemo[1])
     }
-  }, [childrenDemo])
+  }, [])
 
   const data = [
-    { name: 'Girl', value: female },
+    { name: 'Girl', value:female},
     { name: 'Boy', value: male },
   ];
-
+ 
   return (
     <Grid item>
-      <Typography variant="h5" align="center" gutterBottom>
+    <Box display="flex" justifyContent="center" alignItems="center">
+      <Typography variant="h5" gutterBottom style={{color: "#3f51b5"}}>
         Children Demographic
       </Typography>
-      <PieChart width={400} height={360}>
-        <Pie data={data} cx={200} cy={200} labelLine={false} label={renderCustomizedLabel} outerRadius={80} fill="#8884d8" dataKey="value">
-            {
-                data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-            }
+    </Box>
+      <PieChart width={400} height={343}>
+        <Pie
+          data={data}
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+          label={renderCustomizedLabel}
+          labelLine={false}
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORS[index % COLORS.length]}
+              stroke={COLORS[index % COLORS.length]}
+              strokeWidth={2}
+            />
+          ))}
         </Pie>
-        <Legend verticalAlign="top" height={36}/>
+        <Tooltip />
+        <Legend
+          verticalAlign="top"
+          height={36}
+          wrapperStyle={{ fontWeight: "bold", fontSize: "1.2rem", color: "#3f51b5" }}
+        />
       </PieChart>
-    </Grid>
+  </Grid>
   );
 }
