0f8952f2ab723f9f262cdaeafe8f585ec46ec4e9
Jawde2000 <65705404+Jawde2000@users.noreply.github.com>
2023-02-08 01:36:48 +0800

    Update RankingBoard.jsx

--- etix-admin/src/components/adminMenu/RankingBoard.jsx
+++ etix-admin/src/components/adminMenu/RankingBoard.jsx
@@ -1,14 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core';
+import { Typography, } from '@mui/material';
 import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 
 const useStyles = makeStyles(() => ({
   root: {
     width: '100%',
+    height: "100%",
     overflowX: 'auto',
   },
   table: {
     minWidth: 150,
+    minHeight: 200,
   },
 }));
 
@@ -53,15 +56,21 @@ export default function RankingBoard() {
       <Table className={classes.table}>
         <TableHead>
           <TableRow  display="flex">
-            <TableCell>Rank {<MilitaryTechIcon alignItems="center" justifyContent="center" style={{ color: 'gold'}}/>}</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Score</TableCell>
+            <TableCell align="center" style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+              <Typography variant="h6" style={{fontWeight: "bold"}}>Rank</Typography> {<MilitaryTechIcon style={{ fontSize: '25px', color: 'gold'}}/>}
+            </TableCell>
+            <TableCell>
+              <Typography variant="h6" style={{fontWeight: "bold"}}>Name</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h6" style={{fontWeight: "bold"}}>Score</Typography>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((row) => (
             <TableRow key={row.rank}>
-              <TableCell>{row.rank}</TableCell>
+              <TableCell align="center">{row.rank}</TableCell>
               <TableCell>{row.name}</TableCell>
               <TableCell>{row.score}</TableCell>
             </TableRow>
