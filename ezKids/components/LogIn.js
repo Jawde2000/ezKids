f235d8b9f2cb450bfea5e2143b5e728f60a319b2
Matt <43501106+mattlab7@users.noreply.github.com>
2023-02-14 17:12:24 +0800

    ezkids new

---
+++ ezKids/components/LogIn.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { FormBuilder } from 'react-native-paper-form-builder';
+
+function Register() {
+    const {control, setFocus, handleSubmit} = useForm({
+        defaultValues: {
+            userName: '',
+            userPassword: ''
+        }
+    })
+
+    return (
+        <View>
+            <FormBuilder 
+                control={control} 
+                setFocus={setFocus}
+                formConfigArray={[
+                    {
+                        type: 'text',
+                        name: 'userName',
+
+                        rules: {
+                            required: {
+                                value: true,
+                                message: 'Please enter a username'
+                            }
+                        },
+                        textInputProps: {
+                            label: 'Username'
+                        }
+                    },
+                    {
+                        type: 'password',
+                        name: 'userPassword',
+
+                        rules: {
+                            required: {
+                                value: true,
+                                message: 'Please enter a strong password'
+                            }
+                        },
+                        textInputProps: {
+                            label: 'Password'
+                        }
+                    },
+                ]} 
+                />
+                <Button
+                    mode={'contained'}
+                    onPress={handleSubmit((data) => {
+                        console.log(data);
+                })}>
+                    Submit
+                </Button>
+        </View>
+    )
+}
+
+export default Register;
