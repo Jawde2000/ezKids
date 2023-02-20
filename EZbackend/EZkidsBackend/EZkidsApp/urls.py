cafdbee8498a66820281566e98d806078b8688e3
Matt <43501106+mattlab7@users.noreply.github.com>
2023-02-20 08:48:17 +0800

    Update urls.py

--- EZbackend/EZkidsBackend/EZkidsApp/urls.py
+++ EZbackend/EZkidsBackend/EZkidsApp/urls.py
@@ -266,6 +266,13 @@ urlpatterns = [
     # api path to reset password and send email to user
     path('api/user/resetpass/<str:pk>/',
          views.resetPassword, name='user-reset-pass'),
+    
+    # get children attendance information list
+    path('api/attendances/<str:childID>/',
+         views.getChildAttendance, name='class-classlist'),
+
+    path('api/class/<str:pk>/',
+         views.getClassByID, name='class-classDetails'),
 
 
 ]
