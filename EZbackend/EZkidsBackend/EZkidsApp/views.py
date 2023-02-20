18dfb23b2387c70934b811d78bc673e093a79464
Matt <43501106+mattlab7@users.noreply.github.com>
2023-02-20 09:00:49 +0800

    Update views.py

--- EZbackend/EZkidsBackend/EZkidsApp/views.py
+++ EZbackend/EZkidsBackend/EZkidsApp/views.py
@@ -1142,7 +1142,7 @@ def getChildAttendance(request, childID):
         message = {'detail': 'This child has not attended any classes yet'}
         return Response(message, status=status.HTTP_200_OK) 
 
-@apiview(['GET'])
+@api_view(['GET'])
 def getAttendanceList(request):
     try:
         data = request.data
